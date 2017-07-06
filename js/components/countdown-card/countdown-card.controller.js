269c08ad54f64b1e7ac7a6166669ab838df61122
ChetanPate1 <=>
2017-07-06 17:03:39 +0100

    added new properties to shows obj

--- js/components/countdown-card/countdown-card.controller.js
+++ js/components/countdown-card/countdown-card.controller.js
@@ -9,64 +9,115 @@ function CountdownCardController(firebaseAuth){
 
    vm.shows = [
       {
-         "heading": "Game of Thrones",
-         "subHeading": "Season 7 Episode 1",
+         "series": "Game of Thrones",
          "imgSrc": "http://www.gstatic.com/tv/thumb/tvbanners/12502846/p12502846_b_v8_aa.jpg",
-         "airDate": "July 16, 2017 20:00:00"
+         "airDate": "July 16, 2017 20:00:00",
+         "season": "7",
+         "episode": {
+            "number": "1",
+            "name": "",
+            "seen": false,
+            "airDate": "July 16, 2017 20:00:00"
+         }
       },
       {
-         "heading": "Lucifer",
-         "subHeading": "Season 4 Episode 1",
+         "series": "Lucifer",
          "imgSrc": "https://images-na.ssl-images-amazon.com/images/I/91cm4C%2BALoL._SY679_.jpg",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "4",
+         "episode": {
+            "number": "1",
+            "name": "",
+            "seen": false,
+            "airDate": "October 2, 2017 17:00:00"
+         }
       },
       {
-         "heading": "Silicon Valley",
-         "subHeading": "Season 4 Episode 7",
+         "series": "Silicon Valley",
          "imgSrc": "http://toptvshows.me/images/poster/Silicon%20Valley%20season%204%20poster.jpg",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "4",
+         "episode": {
+            "number": "7",
+            "name": "",
+            "seen": false,
+            "airDate": "July 1, 2017 17:00:00"
+         }
       },
       {
-         "heading": "The Leftovers",
-         "subHeading": "Season 4 Episode 1",
+         "series": "The Leftovers",
          "imgSrc": "http://thenerdrecites.com/wp-content/uploads/2017/03/tlft.png",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "4",
+         "episode": {
+            "number": "1",
+            "name": "",
+            "seen": false,
+            "airDate": "January 1, 2017 17:00:00"
+         }
       },
       {
-         "heading": "Daredevil",
-         "subHeading": "Season 3 Episode 1",
+         "series": "Daredevil",
          "imgSrc": "http://www.btchflcks.com/wp-content/uploads/2015/04/daredevilposter.png",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "3",
+         "episode": {
+            "number": "1",
+            "name": "",
+            "seen": false,
+            "airDate": "January 1, 2018 17:00:00"
+         }
       },
       {
-         "heading": "Suits",
-         "subHeading": "Season 6 Episode 17",
+         "series": "Suits",
          "imgSrc": "https://s-media-cache-ak0.pinimg.com/736x/93/cf/e4/93cfe45ee1fdd7b2d330bb30d48b27f4.jpg",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "6",
+         "episode": {
+            "number": "17",
+            "name": "",
+            "seen": false,
+            "airDate": "July 1, 2017 17:00:00"
+         }
       },
       {
-         "heading": "Bitten",
-         "subHeading": "Season 3 Episode 7",
+         "series": "Bitten",
          "imgSrc": "https://s-media-cache-ak0.pinimg.com/736x/80/9d/75/809d759f29d558e4b73447a9a4463a77--watch-full-episodes-all-episodes.jpg",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "3",
+         "episode": {
+            "number": "7",
+            "name": "",
+            "seen": false,
+            "airDate": "July 1, 2017 17:00:00"
+         }
       },
       {
-         "heading": "Westworld",
-         "subHeading": "Season 1 Episode 10",
+         "series": "Westworld",
          "imgSrc": "http://biiinge.konbini.com/files/2016/11/westworld-music-feat.jpg",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "2",
+         "episode": {
+            "number": "1",
+            "name": "",
+            "seen": false,
+            "airDate": "March 1, 2018 12:00:00"
+         }
       },
       {
-         "heading": "Iron Fist",
-         "subHeading": "Season 2 Episode 1",
+         "series": "Iron Fist",
          "imgSrc": "http://cdn1-www.superherohype.com/assets/uploads/gallery/iron-fist/16508182_1863674427225326_8251850749542215766_n.png",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "2",
+         "episode": {
+            "number": "1",
+            "name": "",
+            "seen": false,
+            "airDate": "July 1, 2017 17:00:00"
+         }
       },
       {
-         "heading": "The Strain",
-         "subHeading": "Season 3 Episode 9",
+         "series": "The Strain",
          "imgSrc": "http://www.impawards.com/tv/posters/strain_ver2.jpg",
-         "airDate": "July 1, 2017 17:00:00"
+         "season": "3",
+         "episode": {
+            "number": "9",
+            "name": "",
+            "seen": false,
+            "airDate": "October 23, 2016 17:00:00"
+         }
       }
    ];
 
