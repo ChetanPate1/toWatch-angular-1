03b0b769b69180b66a357a1fba4ef9ccc5f5a602
ChetanPate1 <=>
2017-07-27 17:23:49 +0100

    navigation rebuilt, responsive

--- js/components/navigation-bar/navigation-bar.controller.js
+++ js/components/navigation-bar/navigation-bar.controller.js
@@ -7,8 +7,17 @@ NavigationBarController.$inject = ['firebaseAuth', '$state'];
 function NavigationBarController(firebaseAuth, $state){
    var vm = this;
 
+   vm.open = false;
+   vm.toggleOpen = toggleOpen;
    vm.userSignedIn = userSignedIn;
    vm.logout = logout;
+
+   function toggleOpen() {
+      if(close == 'close'){
+         vm.open = false;
+      }
+      vm.open = !vm.open;
+   }
 
    function userSignedIn(){
       return firebaseAuth.$getAuth();
--- js/components/shows-countdown-card/shows-countdown-card.directive.js
+++ js/components/shows-countdown-card/shows-countdown-card.directive.js
@@ -32,7 +32,6 @@ function showsCountdownCard() {
       scope.save = save;
       scope.aired = aired;
       scope.countWatched = countWatched;
-      scope.countUnwatched = countUnwatched;
 
       function toggleOpen() {
          scope.open = !scope.open;
@@ -52,20 +51,21 @@ function showsCountdownCard() {
          }
       }
 
-
       function aired(date) {
          if(!date){
             return;
          }
          date = parseInt(date);
          var delta = Math.abs((date - now))/1000;
-         var aired = { aired: date - now < 0 };
-         if( date - now > 0 ){
+         var aired = { aired: date - now + 21600 < 0 };
+
+         if( date - now + 21600 > 0 ){
             aired.by = 'not aired';
          }
          else {
-            aired.by = '-' + Math.floor(delta / 86400) + ' days';
+            aired.by = '-' + Math.ceil(delta / 86400) + ' days';
          }
+
          return aired;
       }
 
@@ -87,18 +87,8 @@ function showsCountdownCard() {
                watchlistobj[index].on.episode = 1;
             }
          }
-         
+
          watchlistobj[index].on.episode = i;
-      }
-
-      function countUnwatched(seasoninfo, watchlistobj, index) {
-         var i = 1, size = seasoninfo.length;
-
-         for (i; i < size; i++) {
-            if(!seasoninfo[i].watched && (seasoninfo[i].airDate - now < 0) ){
-               return i;
-            }
-         }
       }
    }
    return directive;
