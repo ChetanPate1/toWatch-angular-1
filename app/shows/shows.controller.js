d2ff41a006809670d2f28ac4c28a7c615d832d6f
ChetanPate1 <=>
2017-07-31 19:14:34 +0100

    loader for search create and functional, layout of shows changed

--- app/shows/shows.controller.js
+++ app/shows/shows.controller.js
@@ -9,23 +9,30 @@ function ShowsController(firebaseArray, episodateApi, helperFunctions, $timeout)
 
    vm.shows = firebaseArray.getByRef('shows');
    vm.add = add;
+   vm.sendStatus = {
+      disableButton: false,
+      loader: false
+   }
 
    function add() {
       var series = vm.series;
       if (vm.series) {
+         vm.sendStatus.disableButton = true;
+         vm.sendStatus.loader = true;
+
          series = helperFunctions.spacesToDashes(series);
-
-         episodateApi.getShow(series)
-            .then(function(showData) {
-
+         episodateApi.getShow(series).then(function(showData) {
+            $timeout(function() {
                if (showData.seasons) {
-                  vm.h = showData;
                   firebaseArray.save('shows', showData);
+                  vm.series = '';
                }else {
 
                }
-            }
-         );
+               vm.sendStatus.disableButton = false;
+               vm.sendStatus.loader = false;
+            }, 2000);
+         });
       }
    }
 }
