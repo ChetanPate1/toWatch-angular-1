d6573e497cdd321e04dbf915c250a87c4df7b415
ChetanPate1 <=>
2017-08-09 15:00:07 +0100

    master now for serving

--- README.md
+++
@@ -1,8 +0,0 @@
-# toWatch
-Tracks shows and air dates
-
-## Stack
-Angular & Firebase
-
-## Screenshot
-[Watchlist page](https://github.com/ChetanPate1/toWatch/blob/master/screenshot-watchlist.jpg)
--- app/services/firebase-auth.factory.js
+++
@@ -1,9 +0,0 @@
-angular
-   .module('app')
-   .factory('firebaseAuth', firebaseAuth);
-
-firebaseAuth.$inject = ['$firebaseAuth'];
-
-function firebaseAuth($firebaseAuth){
-   return $firebaseAuth();
-}
