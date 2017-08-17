29e38680adeba59fbb1d33e72e8328f0048692f4
ChetanPate1 <=>
2017-08-17 13:23:54 +0100

    sign in validation added

--- app/signin/signin.controller.js
+++ app/signin/signin.controller.js
@@ -2,10 +2,15 @@ angular
    .module('app')
    .controller('SigninController', SigninController);
 
-SigninController.$inject = ['firebaseAuth', '$state'];
+SigninController.$inject = ['firebaseAuth', '$state', '$timeout'];
 
-function SigninController(firebaseAuth, $state){
+function SigninController(firebaseAuth, $state, $timeout){
    var vm = this;
+
+   vm.validation = {
+      message: 'Something went wrong',
+      show: false
+   };
 
    vm.login = login;
    vm.signup = signup;
@@ -16,11 +21,15 @@ function SigninController(firebaseAuth, $state){
          password: vm.password
       }
 
+      if (!vm.email || !vm.password) {
+         return;
+      }
+
       firebaseAuth.$signInWithEmailAndPassword(vm.email, vm.password)
          .then(function(firebaseUser) {
             $state.go('shows');
          }).catch(function(error) {
-            console.log(error);
+            validation(error);
          }
       );
    }
@@ -35,8 +44,25 @@ function SigninController(firebaseAuth, $state){
          .then(function(firebaseUser) {
             $state.go('shows');
          }).catch(function(error) {
-            console.log(error);
+            validation(error);
          }
       );
+   }
+
+   function validation(error) {
+      vm.validation.show = true;
+      if (error.code === 'auth/wrong-password') {
+         vm.validation.message = 'Incorrect password';
+      }
+      if(error.code === 'auth/user-not-found'){
+         vm.validation.message = 'User does not exist';
+      }
+      if(error.code === 'auth/email-already-in-use'){
+         vm.validation.message = 'User already exists';
+      }
+
+      $timeout(function() {
+         vm.validation.show = false;
+      }, 3000);
    }
 }
